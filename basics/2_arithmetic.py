989a5b0e516d3dd4fe59e325cac67daeb02f618c
M A Harish J <mr.horror3737@gmail.com>
2023-10-10 10:30:39 +0530

    arith10102023

---
+++ basics/2_arithmetic.py
@@ -0,0 +1,23 @@
+#addition of two nos.
+print(0+1)
+
+#subtraction of two nos.
+print(1729-9271)
+
+#multiplication of two nos.
+print(369*963)
+
+#Division of two nos.
+print(299792458/9)
+
+#Floor Division of two nos.
+print(299792458//9)
+
+#Modulo of a no.
+print(357%3)
+
+#Power of a number
+print(10**10)
+
+#fractional exponent of nos
+print(100**(1/2))
