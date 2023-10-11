58eb1858bb2320b26537a2e7da081089be86ce99
M A Harish J <mr.horror3737@gmail.com>
2023-10-11 06:32:46 +0000

    ifelse

---
+++ statements/1_if_else.py
@@ -0,0 +1,22 @@
+#This script is to validate the python if else satatements
+
+#if stmt
+if True:
+    print('True stmt')
+
+#if else stmt
+x=1
+if x==10:
+    print("x is 10")
+else
+    print("x is not 10")
+
+#if elif else
+if x>0:
+    print('+ve')
+elif x<0:
+    print('-ve')
+else:
+    print('zero')
+
+
