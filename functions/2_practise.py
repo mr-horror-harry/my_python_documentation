ac90a9e001b9f81f42a401df159d24dabec5f073
M A Harish J <mr.horror3737@gmail.com>
2023-10-12 06:37:40 +0000

    practise 10122023/1208

--- functions/2_practise.py
+++ functions/2_practise.py
@@ -1,5 +1,3 @@
-'''
-
 # (1) LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even, but returns the greater if one or both numbers are odd
 def less2Even(x,y):
     if x%2==0 and y%2==0:
@@ -57,7 +55,7 @@ def reversal(s):
 print(reversal(input("Enter a word:\n")))
 
 
-'''
+
 
 
 
