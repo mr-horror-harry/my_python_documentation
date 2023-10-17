1f20df80310b5c51d426d7226c7c7af164cce8bf
M A Harish J <mr.horror3737@gmail.com>
2023-10-17 12:54:27 +0000

    inhert

--- oops/inheritence.py
+++ oops/inheritence.py
@@ -7,15 +7,20 @@ class SuperClass():
         self.regno=regno
 
     def printDetails(self):
-        print(self.name, self.name, "from the super class")
+        print(self.name, self.regno, "from the super class")
 
-class SubClass(SuperClass):
+class SubClass():
     
-    def __init__(self, gender):
-        SuperClass.__init__()
+    def __init__(self, SuperClass, name, regno,  gender):
+        SuperClass.__init__(name, regno)
         self.gender=gender
 
     def printDetails(self):
-        print(self.)
+        print(SuperClass.name, SuperClass.regno,"from the super class", self.gender, "from the sub class")
 
+spr = SuperClass('Harry', 2012094)
+spr.printDetails()
+
+sub = SubClass(spr, 'Horror', 36937, 'Male')
+sub.printDetails()
 
