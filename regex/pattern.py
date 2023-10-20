57844dedc5b26ac62f5a6202e5e07e83cf62b2bf
M A Harish J <mr.horror3737@gmail.com>
2023-10-20 17:54:08 +0530

    regexx half

---
+++ regex/pattern.py
@@ -0,0 +1,11 @@
+import re 
+
+sent="the brown little fox jumped over the lazy dog!"
+word1="fox"
+
+print(re.search(word1, sent))
+print(re.search(word1, sent).span())
+print(re.search(word1, sent).group())
+
+word2='the'
+print(re.findall(word2, sent))
