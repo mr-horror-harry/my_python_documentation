76720199e0421a413a961613c68bee7004cdbe42
M A HARISH J <mr.horror3737@gmail.com>
2023-10-23 20:56:03 +0530

    timer

--- data_handling/pdf/1_read.py
+++ data_handling/pdf/1_read.py
@@ -1,6 +1,10 @@
 import PyPDF2
 
-file = open('./sample.pdf')
+file = open('./sample.pdf', mode='rb')
 
 data = PyPDF2.PdfReader(file)
-print(data.numLines) 
+print(len(data.pages)) 
+
+pg5 = data.pages[4] 
+pg5_txt = pg5.extract_text()
+print(pg5_txt)
--- regex/1_word.py
+++ regex/1_word.py
@@ -3,12 +3,15 @@ import re
 sent="the brown little fox jumped over the lazy dog!"
 word1="fox"
 
+#search() : to find the first occurence of the given keyword
 print(re.search(word1, sent))
 print(re.search(word1, sent).span())
 print(re.search(word1, sent).group())
 
+#findall() : to look for count of given keyword entirely 
 word2='the'
 print(re.findall(word2, sent))
+
 
 li = re.finditer(word2, sent)
 for i in li:
