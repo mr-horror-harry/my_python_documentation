0840347ade4905fa90001043d7e45c76cba6d110
M A Harish J <mr.horror3737@gmail.com>
2023-10-24 09:25:42 +0530

    old

--- data_handling/csv/4_all.py
+++ data_handling/csv/4_all.py
@@ -3,6 +3,7 @@ import csv
 file = open('./wholesale-trade-survey-june-2023-quarter-csv.csv')
 data = csv.reader(file)
 li = list(data)
+file.close()
 
 file2 = open('./written.csv', mode='w+', newline='')
 writer = csv.writer(file2, delimiter=':')
@@ -10,7 +11,12 @@ writer = csv.writer(file2, delimiter=':')
 for line in li[:50]:
     writer.writerow(line)
 
-# f=open('./written.csv')
+file2.seek(0) #to move the file pointer to the beginning
+
+# f=open('./written.csv', mode='r') 
+# or close and open to move pointer to initial
+
 data_read = csv.reader(file2)
-li=list(data_read)
-print(li)
+print(list(data_read))
+
+file2.close()
